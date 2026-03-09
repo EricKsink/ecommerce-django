6a5f752bbbd2a8b02bac90f62fecc5b29da0217b
EricKsink <ericjosuehetnandezduran@gmail.com>
2026-03-08 23:47:11 -0600

    Implementacion de activacion de Cuenta, hasta vid 179

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,3 +136,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR : 'danger',
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ericksink2003@gmail.com'
+EMAIL_HOST_PASSWORD = 'yzbs hvzu nswt zwct'
+EMAIL_USE_TLS = True
